e0a9b0ed07545612de8405001faf871ec7c636bf
Trevor800 <jeynator780@gmail.com>
2023-06-26 13:33:05 -0700

    Trevor algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,37 +1,46 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending
- * order using Bubble Sort
- * @array: The array to be sorted
- * @size: The size of the array
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int temp;
-int swapped;
+	size_t i, len = size;
+	bool bubbly = false;
 
-if (array == NULL || size < 2)
-return;
-for (i = 0; i < size - 1; i++)
-{
-swapped = 0;
+	if (array == NULL || size < 2)
+		return;
 
-for (j = 0; j < size - 1 - i; j++)
-{
-if (array[j] > array[j + 1])
-{
-temp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = temp;
-swapped = 1;
-print_array(array, size);
-}
-}
-
-if (!swapped)
-break;
-}
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
 }
