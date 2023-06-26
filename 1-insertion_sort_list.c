78f0d9070e244954bbf51438b3876b956605a99d
Trevor800 <jeynator780@gmail.com>
2023-06-26 16:02:58 -0700

    Trevor algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order using Insertion Sort
+ * @list: Pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+
+    listint_t *current = (*list)->next;
+
+    while (current != NULL)
+    {
+        listint_t *insertion = current;
+        int value = insertion->n;
+
+        while (insertion->prev != NULL && insertion->prev->n > value)
+        {
+            insertion->n = insertion->prev->n;
+            insertion = insertion->prev;
+            print_list(*list);
+        }
+
+        insertion->n = value;
+        current = current->next;
+    }
+}
 
