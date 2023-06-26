e593e946d2ab1d5ac5d695ed67ccdffb3371e843
Trevor800 <jeynator780@gmail.com>
2023-06-26 15:53:13 -0700

    Trevor algorithm

--- sort.h
+++ sort.h
@@ -1,3 +1,8 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -7,13 +12,16 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
-void print_array(const int *array, size_t size);
+
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+
+
+
+#endif
