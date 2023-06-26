2acb5c5ae8fdb1096d38b11486db558ac9f1efd6
Trevor800 <jeynator780@gmail.com>
2023-06-26 12:40:55 -0700

    Trevor algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp;
+    int swapped;
+
+    if (array == NULL || size < 2)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        swapped = 0;
+
+        for (j = 0; j < size - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swapped = 1;
+                print_array(array, size);
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+Sorting algorithms & Big O
+Our files print_array.c and print_list.c (containing the print_array and print_list functions) will be compiled with your functions during the correction.
+Please declare the prototype of the functions print_array and print_list in your sort.h header file
+Please, note this format is used for Quiz and Task questions.
+
+O(1)
+O(n)
+O(n!)
+n square -> O(n^2)
+log(n) -> O(log(n))
+n * log(n) -> O(nlog(n))
+n + k -> O(n+k)
+…
+Please use the “short” notation (don’t use constants). Example: O(nk) or O(wn) should be written O(n). If an answer is required within a file, all your answers files must have a newline at the end
