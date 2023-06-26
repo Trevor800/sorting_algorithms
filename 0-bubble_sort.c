cbadfa9db1645be47cbede902457412a7ded1508
Trevor800 <jeynator780@gmail.com>
2023-06-26 13:00:35 -0700

    Trevor algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,36 +2,36 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
+ * bubble_sort - Sorts an array of integers in ascending
+ * order using Bubble Sort
  * @array: The array to be sorted
  * @size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+size_t i, j;
+int temp;
+int swapped;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swapped = 1;
+print_array(array, size);
+}
+}
 
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
-
-        if (!swapped)
-            break;
-    }
+if (!swapped)
+break;
+}
 }
