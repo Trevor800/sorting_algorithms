2d78edd3c4489c576fa6014589f1e549b44afeec
Trevor800 <jeynator780@gmail.com>
2023-06-27 13:23:03 -0700

    this is serious

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,25 +9,25 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int tmp;
+size_t i, j, min_idx;
+int tmp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
+for (i = 0; i < size - 1; i++)
+{
+min_idx = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_idx])
+min_idx = j;
+}
 
-        if (min_idx != i)
-        {
-            tmp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = tmp;
-            print_array(array, size);
-        }
-    }
+if (min_idx != i)
+{
+tmp = array[i];
+array[i] = array[min_idx];
+array[min_idx] = tmp;
+print_array(array, size);
+}
+}
 }
